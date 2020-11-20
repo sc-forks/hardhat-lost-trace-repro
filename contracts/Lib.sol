28ffd5d81c540d80bde2be8d427a3abb4a7340c6
cgewecke <christophergewecke@gmail.com>
2020-11-20 09:03:05 -0800

    Fix whitespace in Lib.sol

--- contracts/Lib.sol
+++ contracts/Lib.sol
@@ -4,8 +4,8 @@ library Lib{
 
   function lib_trace(bytes4 x) public pure {}
 
-	function add(uint base) pure public returns (uint newAmount){
+  function add(uint base) pure public returns (uint newAmount){
     lib_trace(0xaaaaaaaa);
-		return base + 5;
-	}
+    return base + 5;
+  }
 }
